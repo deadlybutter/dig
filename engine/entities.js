570518f311ae0b3bab6f1daa02893c28e397d851
deadlybutter <thedeadlybutter@gmail.com>
2016-10-08 14:25:35 -0400

    Begins implementing engine

---
+++ engine/entities.js
@@ -0,0 +1,74 @@
+class Entity {
+  /**
+   * Create an entity.
+   * @param {Vector2} location - Starting location.
+   */
+  constructor(location) {
+    this.location = location;
+
+    this._privateUpdate = this._privateUpdate.bind(this);
+    this.update = this.update.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  /**
+   * !! Internal update function only a base class should override. !!
+   * Designated for common update logic shared across multiple entities.
+   */
+  _privateUpdate() {
+    this.update();
+  }
+
+  /**
+   * Public update function. Entities should override this
+   * if needed for custom functionality.
+   */
+  update() {}
+
+  /**
+   * Render this entity to the canvas.
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {Grid} grid - Used to translate internal grid values to external values.
+   */
+  render(ctx, grid) {}
+}
+
+class PhysicsEntity extends Entity {
+  /**
+   * Create an entity capable of physics.
+   * @param {Vector2} location - Starting location.
+   * @param {number} maxVelocity - The max velocity this entity can hit.
+   * @param {number} mass - The mass of this entity.
+   */
+  constructor(location, maxVelocity, mass) {
+    super(location);
+    this.maxVelocity = maxVelocity;
+    this.mass = mass;
+
+    this.velocity = new Vector2(0, 0);
+    this.acceleration = new Vector2(0, 0);
+
+    this.addForce = this.addForce.bind(this);
+  }
+
+  /**
+   * Apply a force to this entity.
+   * @param {Vector2} force
+   */
+  addForce(force) {
+    force.divide(this.mass);
+    this.acceleration.add(force);
+  }
+
+  _privateUpdate() {
+    // Update velocity with acceleration
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxVelocity);
+
+    // Update location info with velocity
+    this.location.add(this.velocity);
+
+    // Reset acceleration
+    this.acceleration.scale(0);
+  }
+}
---
+++ engine/util.js
@@ -0,0 +1,137 @@
+class Vector2 {
+  /**
+   * Create a Vector.
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.update = this.update.bind(this);
+  }
+
+  /**
+   * Add two vectors together.
+   * @param {Vector2} vec1
+   * @param {Vector2} vec2
+   * @return {Vector2}  The sum of both vectors.
+   */
+  static Add(vec1, vec2) {
+    return new Vector2(vec1.x + vec2.x, vec1.y + vec2.y);
+  }
+
+  /**
+   * Subtracts two vectors.
+   * @param {Vector2} vec1
+   * @param {Vector2} vec2
+   * @return {Vector2}  The difference of both vectors.
+   */
+  static Sub(vec1, vec2) {
+    return new Vector2(vec1.x - vec2.x, vec1.y - vec2.y);
+  }
+
+  /**
+   * Scales a vector by a scalar.
+   * @param {Vector2} vec
+   * @param {number} scalar
+   * @return {Vector2}  The vector scaled.
+   */
+  static Scale(vec, scalar) {
+    return new Vector2(vec.x * scalar, vec.y * scalar);
+  }
+
+  /**
+   * Divides a vector by a divisor.
+   * @param {Vector2} vec
+   * @param {number} divisor
+   * @return {Vector2}  The vector divded.
+   */
+  static Divide(vec, divisor) {
+    return new Vector2(vec.x / divisor, vec.y / divisor);
+  }
+
+  /**
+   * Update the x & y components of this vector.
+   * @param {Vector2} newVector
+   * @return {Vector2} - updated version of this Vector.
+   */
+  update(newVector) {
+    this.x = newVector.x;
+    this.y = newVector.y;
+    return this;
+  }
+
+  /**
+   * Add the given vector with this one.
+   * @param {Vector2} vec
+   * @return {Vector2} - updated version of this Vector.
+   */
+  add(vec) {
+    return this.update(Vector2.Add(this, vec));
+  }
+
+  /**
+   * Subtract the given vector with this one.
+   * @param {Vector2} vec
+   * @return {Vector2} - updated version of this Vector.
+   */
+  sub(vec) {
+    return this.update(Vector2.Sub(this, vec));
+  }
+
+  /**
+   * Scale this vector with the given scalar.
+   * @param {number} scalar
+   * @return {Vector2} - updated version of this Vector.
+   */
+  scale(scalar) {
+    return this.update(Vector2.Scale(this, scalar));
+  }
+
+  /**
+   * Divide this vector with the given divisor.
+   * @param {number} divisor
+   * @return {Vector2} - updated version of this Vector.
+   */
+  divide(divisor) {
+    return this.update(Vector2.Divide(this, divisor));
+  }
+
+  /**
+   * Get the magnitude of this vector
+   * @return {number} - magnitude of this Vector.
+   */
+  magnitude() {
+    return Math.sqrt((this.x * this.x) + (this.y * this.y));
+  }
+
+  /**
+   * Normalize this vector.
+   * @return {Vector2} - normalized version of this Vector.
+   */
+  normalize() {
+    const mag = this.magnitude();
+    if (mag === 0) return;
+    return this.update(Vector2.Divide(this, mag));
+  }
+
+  /**
+   * Prevent this vectors components from going beyond the given cap.
+   * @param {number} cap
+   * @return {Vector2} - Capped version of this Vector.
+   */
+  limit(cap) {
+    this.x = Math.min(this.x, cap);
+    this.y = Math.min(this.y, cap);
+    return this;
+  }
+}
+
+
+// http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
